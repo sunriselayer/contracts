43d4854364fc76f222b7284870ceec0157ab53b6
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-07-29 17:36:01 +0900

    feat: neutron

---
+++ cosmwasm/contracts/sunrise-swap-adapter-neutron/src/execute/sunrise_swap.rs
@@ -0,0 +1,36 @@
+use crate::error::ContractError;
+use crate::msgs::SunriseSwapMsg;
+use cosmwasm_std::{CosmosMsg, DepsMut, Env, IbcTimeout, MessageInfo, Response};
+use cw_utils::one_coin;
+
+#[cfg(not(feature = "library"))]
+pub fn execute_sunrise_swap(
+    _deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: SunriseSwapMsg,
+) -> Result<Response, ContractError> {
+    let mut response = Response::new();
+    let coin = one_coin(&info)?;
+
+    // If we want to verify sender
+    // https://github.com/axelarnetwork/evm-cosmos-gmp-sample/blob/main/cosmwasm-integration/README.md
+    // but it is not needed here
+
+    // https://docs.axelar.dev/dev/general-message-passing/cosmos-gmp
+
+    response = response.add_message(CosmosMsg::Ibc(cosmwasm_std::IbcMsg::Transfer {
+        channel_id: msg.channel_id,
+        to_address: msg.sunrise_address,
+        amount: coin,
+        timeout: IbcTimeout::with_timestamp(env.block.time.plus_seconds(604_800u64)),
+        memo: Some(msg.memo),
+    }));
+
+    // response = response.add_message(CosmosMsg::Bank(cosmwasm_std::BankMsg::Send {
+    //     to_address: msg.sunrise_address,
+    //     amount: vec![coin],
+    // }));
+
+    Ok(response)
+}
